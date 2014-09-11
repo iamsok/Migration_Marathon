c07d853c49452fbb17ba7533c3bcfd9e92f41d80
David Sok <sokd88@gmail.com>
2014-09-11 10:19:55 -0400

    step 2, add authors to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911140316) do
+ActiveRecord::Schema.define(version: 20140911141827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140911140316) do
     t.string   "title",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "author",     null: false
   end
 
 end
